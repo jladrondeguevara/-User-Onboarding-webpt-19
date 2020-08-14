f98fdf0e3444254f64867b72f087e5d331f47f55
Jeovanni <jeovannildg@gmail.com>
2020-08-13 21:35:03 -0600

    finalized project

---
+++ cypress/integration/form_test.js
@@ -0,0 +1,8 @@
+describe('Testing form input', () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000")
+    })
+    it("", ()=>{
+        cy.visit("http://localhost:3000")
+    })
+});
